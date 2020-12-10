eee9b2f9e946856f1881c2f1c04a1d67b77f96bd
Sophia <async3619@naver.com>
2020-12-10 11:34:10 +0900

    add ascii text splash intro

--- src/app/App.ts
+++ src/app/App.ts
@@ -4,5 +4,12 @@ export default class App {
     async run(configFilePath: string) {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const config = new Configuration(configFilePath);
+
+        console.log(`   ______      __    _            __ 
+  / ____/___ _/ /_  (_)___  ___  / /_
+ / /   / __ \`/ __ \\/ / __ \\/ _ \\/ __/
+/ /___/ /_/ / /_/ / / / / /  __/ /_  
+\\____/\\__,_/_.___/_/_/ /_/\\___/\\__/  
+                                     `);
     }
 }
